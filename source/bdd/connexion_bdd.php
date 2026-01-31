158189b73f188a257bd8ddb1e8abd3e123a66f08
rdjamga1919 <rdjamga@gmail.com>
2026-01-31 21:38:22 +0100

    Oubliez pas de changer le 8889 en 3306 dans la page connexion_bdd.php.php et mettre le mot de passe en vide

--- source/bdd/connexion_bdd.php
+++ source/bdd/connexion_bdd.php
@@ -1,1 +1,19 @@
 <?php
+
+$host = '127.0.0.1';  // 127.0.0.1 force TCP/IP
+$port = 8889;         // Vous changez pour 3306 car vous etes sur windows
+$dbname = 'campus_bien_etre';
+$user = 'root';
+$pass = 'root';           // WAMP vide par défaut, MAMP "root" donc vous laissez vide
+
+try {
+    $bdd = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $pass
+    );
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie !";
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}

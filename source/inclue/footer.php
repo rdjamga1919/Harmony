669d81f2340cd515f4ff7a940d323e6b6cab4edc
anais <anaiskg@outlook.fr>
2026-02-03 16:23:32 +0100

    refactor(): debut code footer

--- source/inclue/footer.php
+++ source/inclue/footer.php
@@ -1,2 +1,23 @@
 <?php
-require_once(__DIR__ . '/../../config.php');
+require_once(__DIR__ . '/../../config.php');?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Campus bien-être</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<!-- FOOTER -->
+<footer class="bg-dark text-white py-4 mt-5">
+    <div class="container text-center">
+        <p class="mb-2 fs-5">© 2025 – Tous droits réservés Rosie Djmaga - Anaïs Kriegel--Grapain</p>
+
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>

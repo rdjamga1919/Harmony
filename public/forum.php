7e529cf43053339300e49fed36f72e5ccee2f60e
anais <anaiskg@outlook.fr>
2026-02-17 14:00:24 +0100

    refactor(): ajout du link pour relier le fichier de css au fichier html pour le forum

--- public/forum.php
+++ public/forum.php
@@ -8,7 +8,7 @@ require_once(__DIR__ . '/../config.php');?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Campus bien-être</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href=".css">
 </head>
 <body>
 

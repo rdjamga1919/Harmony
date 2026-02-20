3fab580409a705a0b854f12662192dc30245a8c9
anais <anaiskg@outlook.fr>
2026-02-20 15:37:41 +0100

    refactor, design(): ajout de design et ajout et modification de ligne de code dans tout ces fichiers

--- source/inclue/footer.php
+++ source/inclue/footer.php
@@ -8,6 +8,7 @@ require_once(__DIR__ . '/../../config.php');?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Campus bien-être</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="footer.css">
 </head>
 <body>
 <!-- FOOTER -->
--- source/inclue/header.php
+++ source/inclue/header.php
@@ -7,12 +7,13 @@ require_once(__DIR__ . '/../../config.php');?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Campus Bien-Être</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="header.css">
 </head>
 <body>
 
 <!-- NAVBAR MODERNE -->
-<nav class="navbar navbar-expand-lg text-white py-4 mt-5" style="background-color: #c9a0a0;">
-    <div class="container">
+<nav class="navbar navbar-expand-lg text-white py-4" style="background-color: #c9a0a0;">
+    <div class="container-fluid">
         <a class="navbar-brand fs-3 fw-bold text-white" href="#">Harmony</a>
 
         <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#menuBurger">
@@ -32,9 +33,6 @@ require_once(__DIR__ . '/../../config.php');?>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-white" href="#meslivres">📞 Contact </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#meslivres"></a>
                 </li>
             </ul>
         </div>
